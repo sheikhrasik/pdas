127eae3965164af9a50c3e692d2b3af4657b2e64
sheikhrasik <sheikhrasik101@gmail.com>
2022-11-27 11:51:04 +0530

    added the final styling to the app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "bulma/css/bulma.css";
+import ProfileCard from "./ProfileCard";
+import SiriImage from "./images/siri.png";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <p className="title">Personal Digital Assitants</p>
+        </div>
+      </section>
+
+      <div className="container">
+        <section className="section">
+          <div className="columns">
+            <div className="column is-4">
+              <ProfileCard 
+              title="Siri" 
+              handle="@siri007" 
+              image={SiriImage} 
+              description="Siri was made by Apple Inc. and is phased out."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Alexa"
+                handle="@alexa903"
+                image={AlexaImage}
+                description="Alexa was created by Amazon and does some stuff."
+              />
+            </div>
+            <div className="column is-4">
+              <ProfileCard
+                title="Cortana"
+                handle="@cortana33"
+                image={CortanaImage}
+                description="Cortana was made by Microsoft and we don't know much about it"
+              />
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
